552145c7ec6430bb3b2f775995b57d78fedeaf55
Nathan Pearson <npearson72@gmail.com>
2018-04-05 20:14:38 +0300

    Update Brewfile with conditionals

--- ansible/roles/zsh/files/env.zsh
+++ ansible/roles/zsh/files/env.zsh
@@ -3,4 +3,6 @@
 export VISUAL=nvim
 export GIT_EDITOR=$VISUAL
 export EDITOR=$VISUAL
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+GNU_PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin"
+export MANPATH="$GNU_PATH:$MANPATH"
--- ansible/roles/zsh/files/path.zsh
+++ ansible/roles/zsh/files/path.zsh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$HOME/bin:$HOME/rbenv/bin:$PATH"
+GNU_PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin"
+export PATH="$GNU_PATH:$HOME/bin:$HOME/rbenv/bin:$PATH"
