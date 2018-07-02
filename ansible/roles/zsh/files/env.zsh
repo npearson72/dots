3c0802f05fc10d3980a9515cecf7cd9ad63c33d9
Nathan Pearson <npearson72@gmail.com>
2018-07-02 04:18:44 +0300

    Update additional settings

--- ansible/roles/zsh/files/env.zsh
+++ ansible/roles/zsh/files/env.zsh
@@ -3,6 +3,3 @@
 export VISUAL=nvim
 export GIT_EDITOR=$VISUAL
 export EDITOR=$VISUAL
-
-GNU_PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin"
-export MANPATH="$GNU_PATH:$MANPATH"
--- ansible/roles/zsh/files/path.zsh
+++ ansible/roles/zsh/files/path.zsh
@@ -2,4 +2,5 @@
 
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 GNU_PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin"
-export PATH="$GNU_PATH:$HOME/bin:$HOME/rbenv/bin:$PATH"
+export MANPATH="$GNU_PATH:$MANPATH"
+export PATH="$MANPATH:$HOME/bin:$HOME/rbenv/bin:$PATH"
