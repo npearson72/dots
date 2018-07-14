9e1b56f1aaccee7e3688424cc4cc19c3a7fcfd61
Nathan Pearson <npearson72@gmail.com>
2018-07-14 19:24:13 +0300

    Update colors

--- ansible/roles/zsh/files/env.zsh
+++ ansible/roles/zsh/files/env.zsh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 export VISUAL=nvim
 export GIT_EDITOR=$VISUAL
 export EDITOR=$VISUAL
--- ansible/roles/zsh/files/path.zsh
+++ ansible/roles/zsh/files/path.zsh
@@ -2,5 +2,7 @@
 
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 GNU_PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin"
-export MANPATH="$GNU_PATH:$MANPATH"
-export PATH="$MANPATH:$HOME/bin:$HOME/rbenv/bin:$PATH"
+export PATH="$GNU_PATH:$HOME/bin:$HOME/rbenv/bin:$PATH"
+
+MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+export MANPATH
