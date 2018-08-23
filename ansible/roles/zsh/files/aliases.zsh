914503a1dcc5e2a63c2faa1c6a1aa7b37fdb324c
Nathan Pearson <nathan.pearson@acimacredit.com>
2018-08-23 12:56:00 -0700

    Add rancher_console function

--- ansible/roles/zsh/files/aliases.zsh
+++ ansible/roles/zsh/files/aliases.zsh
@@ -8,3 +8,4 @@ if type nvim > /dev/null 2>&1; then
 fi
 alias reload!="source $HOME/.zshrc"
 alias start_rails="bundle exec rails s -b 0.0.0.0"
+alias rc="rancher_console"
---
+++ ansible/roles/zsh/functions/rancher_console.zsh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function rancher_console {
+  if [ $1 = production ]
+  then
+    export RANCHER_URL=$RANCHER_URL_PRODUCTION
+    export RANCHER_ACCESS_KEY=$RANCHER_ACCESS_KEY_PRODUCTION
+    export RANCHER_SECRET_KEY=$RANCHER_SECRET_KEY_PRODUCTION
+  else if [ $1 = staging ]
+    export RANCHER_URL=$RANCHER_URL_STAGING
+    export RANCHER_ACCESS_KEY=$RANCHER_ACCESS_KEY_STAGING
+    export RANCHER_SECRET_KEY=$RANCHER_SECRET_KEY_STAGING
+  fi
+
+  id=$(rancher ps -c | grep -i $2"-api-[0-9]" | cut -f 1 -d' ' | head -n 1)
+  rancher exec -it $id bash
+}
