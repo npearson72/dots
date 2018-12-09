1dd9671fae8b49e2fbd0d3acd172f0dfd84d5193
Nathan Pearson <npearson72@gmail.com>
2018-12-10 01:51:25 +0200

    Add tmux

--- ansible/roles/zsh/functions/git.zsh
+++ ansible/roles/zsh/functions/git.zsh
@@ -1,29 +1,25 @@
 #!/bin/sh
 
 git_fzf_show() {
-  git log --graph --color=always \
-      --format="%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-      --bind "ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {} FZF-EOF"
+  format="%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr) \
+    %C(bold blue)<%an>%Creset"
+
+  bind="ctrl-m:execute: (grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+  {} FZF-EOF"
+
+  git log --graph --color=always --format=$format "$@" | fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort --bind $bind
 }
 
-# Git overwrites
 git() {
-    if [ "$1" = "add" ] && [ -z "$2" ] # git add using fzf
-    then
-        command git add $(git ls-files -m -o --exclude-standard | fzf -m)
-    elif [ "$1" = "rm" ] && [ -z "$2" ] # git rm using fzf
-    then
-        command git rm $(git ls-files -d --exclude-standard | fzf -m)
-    elif [ "$1" = "reset" ] && [ -z "$2" ] # git reset using fzf
-    then
-        command git diff --name-only --cached | fzf -m | xargs -I {} sh -c 'git reset -- $0' {} > /dev/null 2>&1
-    elif [ "$1" = "show" ] && [ "$2" -eq 0 ] # git commit browser
-    then
-        git_fzf_show
-    else
-        command git $@
-    fi;
+  if [ "$1" = "add" ] && [ -z "$2" ]; then
+    command git add $(git ls-files -m -o --exclude-standard | fzf -m)
+  elif [ "$1" = "rm" ] && [ -z "$2" ]; then
+    command git rm $(git ls-files -d --exclude-standard | fzf -m)
+  elif [ "$1" = "reset" ] && [ -z "$2" ]; then
+    command git diff --name-only --cached | fzf -m | xargs -I {} sh -c 'git reset -- $0' {} > /dev/null 2>&1
+  elif [ "$1" = "show" ] && [ "$2" -eq 0 ]; then
+    git_fzf_show
+  else
+    command git $@
+  fi
 }
---
+++ ansible/roles/zsh/functions/tmux.zsh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+tmux() {
+  if [ "$1" = "menu" ]; then
+    if [ ! -n "$TMUX" ] && [[ $(tmux ls 2> /dev/null | head -n 1) == *"created"* ]]; then
+      list=$(tmux ls 2> /dev/null | grep -v attached)
+      if [ -n "$list" ]; then
+        selection=$(tmux ls 2> /dev/null | grep -v attached | fzf | cut -f 1 -d " ")
+        if [ -n "$selection" ]; then
+          tmux -f $HOME/.config/.tmux.conf attach -t ${selection: : -1}
+        fi
+      fi
+    else
+      command tmux -f $HOME/.config/.tmux.conf new -s default > /dev/null 2>&1
+    fi
+  else
+    command tmux -f $HOME/.config/.tmux.conf $@
+  fi
+}
