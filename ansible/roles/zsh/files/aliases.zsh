cf2a8cdaffcecddda1045d7ac0c0bab2928fc6f6
Nathan Pearson <npearson72@gmail.com>
2018-12-13 22:46:29 +0200

    Update reload! alias

--- ansible/roles/zsh/files/aliases.zsh
+++ ansible/roles/zsh/files/aliases.zsh
@@ -6,7 +6,7 @@ if type nvim > /dev/null 2>&1; then
   alias vim=nvim
   alias vi=nvim
 fi
-alias reload!="source $HOME/.zshrc"
+alias reload!='exec zsh -l'
 alias start_rails='bundle exec rails s -b 0.0.0.0'
 alias rc='rancher_console'
 alias mux='tmuxinator'
