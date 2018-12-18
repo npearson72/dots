5b76c41ba5bf3e1ea44c3b6a966d31fa7dd15c90
Nathan Pearson <npearson72@gmail.com>
2018-12-18 15:12:51 +0200

    Add keep_awake function

---
+++ ansible/roles/zsh/functions/keep_awake.zsh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+_get_caffeinated_pid() {
+  ps aux | grep caffeinate | grep -v grep | tr -s ' ' | sed -z 's/\012/ /g' | rev | cut -d ' ' -f 1 | rev
+}
+
+_get_caffeinated_apps() {
+  xargs -I {} sh -c 'ps -p {} -o comm=' | rev | cut -d / -f 1 | rev
+}
+
+_keep_awake_status() {
+  apps=$(_get_caffeinated_pid | _get_caffeinated_apps)
+
+  if [ -n "$apps" ]; then
+    echo "The following apps are caffeinated:\n$apps"
+  else
+    echo "No apps appear to be caffeinated"
+  fi
+}
+
+keep_awake() {
+  if [ $1 = "--status" ]; then
+    _keep_awake_status
+  else
+    pid=$(ps aux | grep -i $1 | grep -v grep | tr -s ' ' | cut -d ' ' -f 2)
+
+    if [ -n "$pid" ]; then
+      caffeinate -w $pid &
+    else
+      echo "$1 does not appear to be running"
+    fi
+  fi
+}
