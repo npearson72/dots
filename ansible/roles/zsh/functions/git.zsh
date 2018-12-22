8ecac31ac60cef65f8b3b02f1c337eb966a25536
Nathan Pearson <nathan.pearson@acimacredit.com>
2018-12-21 17:37:59 -0700

    Enhances git fzf stash and show functions

--- ansible/roles/zsh/functions/git.zsh
+++ ansible/roles/zsh/functions/git.zsh
@@ -18,27 +18,39 @@ _git_fzf_checkout_branch() {
 
 _git_fzf_show() {
   git log\
-    --graph\
     --color=always\
-    --format="%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset" $@ |\
+    --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset' |\
   fzf\
     --ansi\
     --tiebreak=index\
-    --header "(press enter to preview)"\
-    --bind "enter:execute: (grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+    --header "<enter> view commit | <ctrl-p> toggle preview window | <esc> quit"\
+    --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %' | diff-so-fancy"\
+    --preview-window "hidden"\
+    --bind "ctrl-p:toggle-preview"\
+    --bind "enter:execute: (grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %' | diff-so-fancy | less -R) << 'FZF-EOF'
+  {} FZF-EOF"
+}
+
+_git_fzf_stash_list() {
+  git stash list\
+    --color=always\
+    --pretty=format:'%Cred%gd%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)' |\
+  fzf\
+    --ansi\
+    --tiebreak=index\
+    --header "<enter> view stash | <alt-enter> apply stash | <alt-d> drop stash | <ctrl-p> toggle preview window | <esc> quit"\
+    --preview "echo {} | grep -o 'stash@{[0-9]}' | head -1 | xargs -I % sh -c 'git show --color=always %' | diff-so-fancy"\
+    --preview-window "hidden"\
+    --bind "ctrl-p:toggle-preview"\
+    --bind "alt-d:execute(echo {} | grep -o 'stash@{[0-9]}' | xargs -I % sh -c 'git stash drop %')+abort"\
+    --bind "alt-enter:execute(echo {} | grep -o 'stash@{[0-9]}' | xargs -I % sh -c 'git stash apply %')+abort"\
+    --bind "enter:execute: (grep -o 'stash@{[0-9]}' | xargs -I % sh -c 'git stash show -p --color=always %' | diff-so-fancy | less -R) << 'FZF-EOF'
   {} FZF-EOF"
 }
 
 _git_fzf_stash_apply() {
   if [ $(git stash list | wc -l) -gt 1 ]; then
-    git stash list |
-    fzf\
-      --ansi\
-      --tiebreak=index\
-      --header "(press enter to preview or alt-enter to apply)"\
-      --bind "alt-enter:execute(echo {} | grep -o 'stash@{[0-9]}' | xargs -I % sh -c 'git stash apply %')+abort"\
-      --bind "enter:execute: (grep -o 'stash@{[0-9]}' | xargs -I % sh -c 'git stash show -p --color=always % | less -R') << 'FZF-EOF'
-    {} FZF-EOF"
+    _git_fzf_stash_list
   else
     git stash apply stash@{0}
   fi
@@ -59,7 +71,7 @@ git() {
       cob)
         _git_fzf_checkout_branch
         ;;
-      show)
+      show|lg)
         _git_fzf_show
         ;;
       *)
@@ -67,6 +79,9 @@ git() {
     esac
   elif [ $1 = stash ] && [ -z $3 ]; then
     case $2 in
+      ls)
+        _git_fzf_stash_list
+        ;;
       apply)
         _git_fzf_stash_apply
         ;;
