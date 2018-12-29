819252fe952bd0f274f1c2aedf146cfe3b794226
Nathan Pearson <npearson72@gmail.com>
2018-12-29 14:09:39 +0200

    Update fzf settings for custom git function

--- ansible/roles/zsh/functions/git.zsh
+++ ansible/roles/zsh/functions/git.zsh
@@ -22,9 +22,12 @@ _git_fzf_log() {
     --pretty=format:'%Cred%h%Creset - %s%C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset' $@ |\
   fzf\
     --ansi\
-    --header "<enter> view commit | <ctrl-p> toggle preview window | <esc> quit"\
+    --no-sort\
+    --tiebreak=index\
+    --header "<enter> view commit | <ctrl-p> toggle preview window | <ctrl-s> sort | <esc> quit"\
     --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs git show --color=always | diff-so-fancy"\
     --preview-window "hidden"\
+    --bind "ctrl-s:toggle-sort"\
     --bind "ctrl-p:toggle-preview"\
     --bind "up:preview-up,down:preview-down,shift-up:preview-page-up,shift-down:preview-page-down"\
     --bind "enter:execute: (grep -o '[a-f0-9]\{7\}' | head -1 | xargs git show --color=always | diff-so-fancy | less -R) <<-FZF-EOF
@@ -39,10 +42,13 @@ _git_fzf_stash_list() {
   fzf\
     --exit-0\
     --ansi\
-    --header "<enter> view stash | <alt-enter> apply stash | <alt-d> drop stash | <ctrl-p> toggle preview window | <esc> quit"\
+    --no-sort\
+    --tiebreak=index\
+    --header "<enter> view stash | <alt-enter> apply stash | <alt-d> drop stash | <ctrl-p> toggle preview window | <ctrl-s> sort | <esc> quit"\
     --preview "echo {} | grep -o 'stash@{[0-9]}' | head -1 | xargs git show --color=always | diff-so-fancy"\
     --preview-window "hidden"\
     --bind "ctrl-p:toggle-preview"\
+    --bind "ctrl-s:toggle-sort"\
     --bind "up:preview-up,down:preview-down,shift-up:preview-page-up,shift-down:preview-page-down"\
     --bind "alt-d:execute(echo {} | grep -o 'stash@{[0-9]}' | xargs git stash drop)+abort"\
     --bind "alt-enter:execute(echo {} | grep -o 'stash@{[0-9]}' | xargs git stash apply)+abort"\
