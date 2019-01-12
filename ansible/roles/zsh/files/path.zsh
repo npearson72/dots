fe254c9abf1c7a1e3ac6ec0b2bf03a7f7fb0c1a4
Nathan Pearson <nathan.pearson@acimacredit.com>
2019-01-11 18:49:54 -0700

    Fix stty issue

--- ansible/roles/zsh/files/path.zsh
+++ ansible/roles/zsh/files/path.zsh
@@ -2,7 +2,8 @@
 
 PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin'
 GNU_PATH='/usr/local/opt/coreutils/libexec/gnubin'
-export PATH="$HOME/bin:$HOME/rbenv/bin:$GNU_PATH:$PATH"
+STTY_HACK='/usr/local/sbin/stty'
+export PATH="$HOME/bin:$HOME/rbenv/bin:$STTY_HACK:$GNU_PATH:$PATH"
 
 MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 export MANPATH
