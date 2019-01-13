024b97cc1946823ff363f3b8b302cbe1a4704fad
Nathan Pearson <npearson72@gmail.com>
2019-01-13 03:13:43 +0200

    Update brew function

--- ansible/roles/zsh/functions/brew.zsh
+++ ansible/roles/zsh/functions/brew.zsh
@@ -4,11 +4,12 @@ brew() {
   if [[ $1 = bundle ]]
   then
     if [[ $@ =~ cleanup ]]; then
-      /usr/local/bin/brew $@ -v --force --zap --file="$HOME/.config/Brewfile"
+      command brew cleanup -s
+      command brew $@ -v --force --zap --file="$HOME/.config/Brewfile"
     else
-      /usr/local/bin/brew $@ -v --file="$HOME/.config/Brewfile"
+      command brew $@ -v --file="$HOME/.config/Brewfile"
     fi
   else
-    /usr/local/bin/brew $@
+    command brew $@
   fi
 }
