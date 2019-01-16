b0f637265d4d2b2137b4a5ac540e24b72ff94670
Nathan Pearson <npearson72@gmail.com>
2019-01-16 15:56:36 +0200

    Clean up stty placement

--- ansible/roles/zsh/files/path.zsh
+++ ansible/roles/zsh/files/path.zsh
@@ -2,7 +2,7 @@
 
 PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin'
 GNU_PATH='/usr/local/opt/coreutils/libexec/gnubin'
-STTY_HACK='/usr/local/sbin/stty'
+STTY_HACK='/usr/local/sbin'
 export PATH="$HOME/bin:$HOME/rbenv/bin:$STTY_HACK:$GNU_PATH:$PATH"
 
 MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
