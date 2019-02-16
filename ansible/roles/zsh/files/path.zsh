8382406b78a485b730ff4dfbfb3c3d66c364a9d7
Nathan Pearson <npearson72@gmail.com>
2019-02-16 16:28:41 +0200

    Update Brewfile(s) and path to work with GNU changes

--- ansible/roles/zsh/files/path.zsh
+++ ansible/roles/zsh/files/path.zsh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin'
-GNU_PATH='/usr/local/opt/coreutils/libexec/gnubin'
 STTY_HACK='/usr/local/sbin'
-export PATH="$HOME/bin:$HOME/rbenv/bin:$STTY_HACK:$GNU_PATH:$PATH"
+GNU_PATH="/usr/local/opt/coreutils/libexec/gnubin:\
+/usr/local/opt/findutils/libexec/gnubin:\
+/usr/local/opt/gnu-sed/libexec/gnubin:\
+/usr/local/opt/gnu-tar/libexec/gnubin:\
+/usr/local/opt/gnu-which/libexec/gnubin"
+
+export PATH="$STTY_HACK:$GNU_PATH:$PATH"
 
 MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 export MANPATH
