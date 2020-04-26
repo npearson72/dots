3ffd02bd8a054ed9c7caddcf95eddeb47a88daa8
Nathan Pearson <npearson72@gmail.com>
2020-04-25 19:54:52 -0700

    Fixes a few issues with ansible install script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ installHomeBrew() {
   if ! [[ $(which brew) ]]; then
     echo "\n=> Installing Homebrew\n"
 
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 }
 
