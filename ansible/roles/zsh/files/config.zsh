24b2365420f3b1a07951ae42b3e2993910da4d52
Nathan Pearson <npearson72@gmail.com>
2020-12-29 08:17:43 +0200

    Updates Brewfile and brings back LS_COLORS

--- ansible/roles/zsh/files/config.zsh
+++ ansible/roles/zsh/files/config.zsh
@@ -8,5 +8,6 @@ source $CONFIG_DIR/defaults.zsh
 source $CONFIG_DIR/aliases.zsh
 source $CONFIG_DIR/bindings.zsh
 source $CONFIG_DIR/completion.zsh
+source $CONFIG_DIR/colors.zsh
 source $CONFIG_DIR/prompt.zsh
 source $CONFIG_DIR/misc.zsh
