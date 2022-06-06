abbaa082de1aefba4fbe1ca1d7ca435c04d85502
Nathan Pearson <npearson72@gmail.com>
2022-06-06 10:38:10 -0700

    Updates mux function

--- ansible/roles/zsh/functions/mux.zsh
+++ ansible/roles/zsh/functions/mux.zsh
@@ -4,8 +4,15 @@ mux() {
   tmuxp_configs=("ams" "bms" "cp" "mp" "lms" "uw" "kipper")
 
   if [[ $1 = "kill" ]]; then
-    tmux list-panes -s -F "#{pane_pid} #{pane_current_command}" -t $2 | grep -v tmux | cut -f 1 -d ' ' | xargs kill
-    tmux kill-session -t $2
+    if [[ $2 = "acima" ]]; then
+      for conf in $tmuxp_configs; do
+        tmux list-panes -s -F "#{pane_pid} #{pane_current_command}" -t $conf | grep -v tmux | cut -f 1 -d ' ' | xargs kill
+        tmux kill-session -t $conf
+      done
+    else
+      tmux list-panes -s -F "#{pane_pid} #{pane_current_command}" -t $2 | grep -v tmux | cut -f 1 -d ' ' | xargs kill
+      tmux kill-session -t $2
+    fi
   elif [[ $1 = "acima" ]]; then
     for conf in $tmuxp_configs; do
       tmuxp load -d $XDG_CONFIG_HOME/tmuxp/$conf.yml
