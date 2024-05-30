27d994a1fd84b53e710363f0f24d72e0eda00519
Nathan Pearson <npearson72@gmail.com>
2024-05-30 00:18:53 -0500

    Cleans up autocommands

--- ansible/roles/neovim/files/nvim/lua/configs/autocommands.lua
+++ ansible/roles/neovim/files/nvim/lua/configs/autocommands.lua
@@ -1,13 +1,21 @@
-vim.cmd([[
-augroup CursorLine
-  autocmd!
-  autocmd VimEnter,WinEnter,BufWinEnter,CursorMoved * setlocal cursorline
-  autocmd WinLeave * setlocal nocursorline
-augroup END
+local create_augroup = vim.api.nvim_create_augroup
+local create_autocmd = vim.api.nvim_create_autocmd
 
-augroup CursorColumn
-  autocmd!
-  autocmd VimEnter,WinEnter,BufWinEnter,CursorMoved * setlocal cursorcolumn
-  autocmd WinLeave * setlocal nocursorcolumn
-augroup END
-]])
+local group = create_augroup('CursorTracker', { clear = false })
+
+create_autocmd({ 'VimEnter', 'WinEnter', 'BufWinEnter', 'CursorMoved' }, {
+  group = group,
+  pattern = '*',
+  command = 'setlocal cursorcolumn cursorline'
+})
+
+create_autocmd('WinLeave', {
+  group = group,
+  pattern = '*',
+  command = 'setlocal nocursorcolumn nocursorline'
+})
+
+create_autocmd('Filetype', {
+  pattern = 'gitcommit',
+  command = 'setlocal spell textwidth=80 colorcolumn=80'
+})
