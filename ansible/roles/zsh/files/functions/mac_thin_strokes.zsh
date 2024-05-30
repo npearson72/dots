53869c6633b8dac9284e5fb454cae0d1c141d71a
Nathan Pearson <npearson72@gmail.com>
2024-05-30 00:37:04 -0500

    Updates mac_thin_strokes function

--- ansible/roles/zsh/files/functions/mac_thin_strokes.zsh
+++ ansible/roles/zsh/files/functions/mac_thin_strokes.zsh
@@ -3,5 +3,7 @@
 mac_thin_stroke() {
   if [[ ! $(defaults read -g CGFontRenderingFontSmoothingDisabled 2> /dev/null) = 0 ]]; then
     defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+  else
+    defaults write -g CGFontRenderingFontSmoothingDisabled -bool YES
   fi
 }
