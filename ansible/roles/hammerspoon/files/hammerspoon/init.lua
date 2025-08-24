b416d86177fdaf150b3723989da319eae9796116
Nathan Pearson <npearson72@gmail.com>
2025-08-23 21:50:45 -0600

    Fixes hammerspoon lua script

--- ansible/roles/hammerspoon/files/hammerspoon/init.lua
+++ ansible/roles/hammerspoon/files/hammerspoon/init.lua
@@ -1,18 +1,21 @@
+local lastFocusedWindow = nil
+
 hs.hotkey.bind({ 'cmd' }, 'escape', function()
   local alacritty = hs.application.find('Alacritty')
 
+  if not alacritty then
+    return
+  end
+
   if alacritty:isFrontmost() then
-    alacritty:hide()
-
-    -- Get all visible windows, ordered from front to back.
-    local windows = hs.window.orderedWindows()
-
-    -- If there's at least one other window behind Alacritty...
-    if #windows > 1 then
-      -- ...focus the second window in the list (the one behind the frontmost).
-      windows[2]:focus()
+    if lastFocusedWindow and lastFocusedWindow:application() and lastFocusedWindow:application():isRunning() then
+      lastFocusedWindow:focus()
     end
+
+    alacritty:hide()
   else
+    lastFocusedWindow = hs.window.focusedWindow()
+
     alacritty:activate()
   end
 end)
