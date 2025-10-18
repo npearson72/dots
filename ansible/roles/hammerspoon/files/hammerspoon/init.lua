fa8153cf6982128aa1063dc4e4f3d4d83a095ddc
Nathan Pearson <npearson72@gmail.com>
2025-10-18 02:30:42 -0600

    Fix hammerspoon lua script

--- ansible/roles/hammerspoon/files/hammerspoon/init.lua
+++ ansible/roles/hammerspoon/files/hammerspoon/init.lua
@@ -1,21 +1,36 @@
+local alacrittyApp = hs.application.find('Alacritty')
 local lastFocusedWindow = nil
 
-hs.hotkey.bind({ 'cmd' }, 'escape', function()
-  local alacritty = hs.application.find('Alacritty')
+-- This watcher is slightly improved to be more reliable.
+-- It checks that the window is a "standard" window, preventing it
+-- from saving palettes or other non-window UI elements.
+local windowWatcher = hs.window.filter.new(false)
+windowWatcher:subscribe(hs.window.filter.windowFocused, function(window, appName)
+  -- We only care about standard windows that are not from Alacritty
+  if window:isStandard() and appName ~= "Alacritty" then
+    lastFocusedWindow = window
+  end
+end)
 
-  if not alacritty then
-    return
+hs.hotkey.bind({ 'cmd' }, 'escape', function()
+  if not alacrittyApp then
+    -- Attempt to find it again in case it was launched after Hammerspoon started
+    alacrittyApp = hs.application.find('Alacritty')
+    if not alacrittyApp then return end
   end
 
-  if alacritty:isFrontmost() then
-    if lastFocusedWindow and lastFocusedWindow:application() and lastFocusedWindow:application():isRunning() then
-      lastFocusedWindow:focus()
-    end
+  if alacrittyApp:isFrontmost() then
+    alacrittyApp:hide()
 
-    alacritty:hide()
+    -- Wait a tiny moment for the OS to finish its "hide" animation
+    -- before we try to change the focus ourselves. This prevents the race condition.
+    hs.timer.doAfter(0.1, function()
+      if lastFocusedWindow and not lastFocusedWindow:isDestroyed() and lastFocusedWindow:application():isRunning() then
+        lastFocusedWindow:focus()
+      end
+    end)
   else
-    lastFocusedWindow = hs.window.focusedWindow()
-
-    alacritty:activate()
+    -- The watcher has already saved our last window, so we just need to activate Alacritty.
+    alacrittyApp:activate()
   end
 end)
